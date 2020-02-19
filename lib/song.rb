2d2ad3cbd8e9cde080287debf37479161e676f0c
Lester Young <lyoung070789@gmail.com>
2020-02-19 19:04:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,10 @@ class Song
   
   end 
   
-  def self.create 
-    @@all << self.new
+  def self.create(name, artist_name)
+    song = self.new
+    @name = name 
+    @artist_name = artist_name
+    @@all << song
   end 
 end
